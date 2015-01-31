bad3ae273fdb7387f1cbfd4f8d8a69e9f0c4db11
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-01-31 21:54:59 +0000

    Fixes

--- config.ml
+++ config.ml
@@ -4,5 +4,4 @@ let main =
   foreign "Unikernel.Main" (console @-> ipv4 @-> job)
 
 let () =
-  Defaults.tap := Param.(literal "tap0" string);
   register "test" [ main $ create_console () $ create_ipv4 (tap ()) ]
